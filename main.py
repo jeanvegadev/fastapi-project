09e555e8bde0ad70e394bcb66d55e81aa0b1452e
jeanvegadev <jcvega.dev@gmail.com>
2023-09-07 12:16:55 -0500

    asyncronous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from typing import Union
+import time
+import asyncio
 
 from fastapi import FastAPI
 
@@ -6,10 +8,17 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
+    await asyncio.gather(*[duerme(), duerme(), duerme()])
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+async def duerme():
+    print("duerme")
+    await asyncio.sleep(5)
+    print("despierta")
